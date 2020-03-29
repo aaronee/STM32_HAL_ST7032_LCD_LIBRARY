47d671824031c43d90a389d88c789a834e0e8992
khoahuynh93 <khoahuynh.ld@gmail.com>
2020-03-28 22:46:15 -0400

    edit some comments

--- STM32_ST7032.h
+++ STM32_ST7032.h
@@ -1,8 +1,23 @@
 /*
- * STM32_ST7036.h
+ *	@File: STM32_ST7032.h
+ *	@This file is free to be used, modified. This header must be remained. Thank you.
  *
- *  Created on: Mar. 27, 2020
- *      Author: AARONEE - khoahuynh
+ *	@Introduction: This is the library written for ST7032 LCD
+ *				> This library is based on STM32 HAL library. You must use STM32 HAL library to use this library
+ *				> The library requires I2C_HandleTypeDef (Master mode) & TIM_HandleTypeDef (PWM mode) to control LCD
+ *				> Some codes are based on ST7306LCD_Library from author @F. Malpartida, and can be found at URL:https://github.com/pkourany/ST7306LCD_Library
+ *	@MainFunction:
+ *				> Initializing ST7032 based LCD in 2 line display 5x8, inst table 1 as default
+ *				> Controlling LCD cursor/blink, screen shift right/left, turn on/off, move cursor to home
+ * 				> Loading custom character (8 characters)
+ * 				> Sending command, char, string to LCD
+ * 				> Adjust contrast, backlight levels (require 1 pin for PWM to control backlight)
+ *	@Bugs/requests: please contact via email, thank you.
+ *
+ *  @Created on: Mar. 27, 2020
+ *  @Author: AARONEE - khoahuynh
+ * 	@Email: khoahuynh.ld@gmail.com
+ * 	@Github: https://github.com/khoahuynhld
  */
 
 #ifndef INC_STM32_ST7032_H_
